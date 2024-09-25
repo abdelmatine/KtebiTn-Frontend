07b0c584c714e8eae909f1acb745904bf0255c9e
abdelmatine <abdelmatinesfar@gmail.com>
2024-09-25 15:43:36 +0200

    final

---
+++ src/app/modules/book/book-routing.module.ts
@@ -0,0 +1,65 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { authGuard } from 'src/app/services/guard/auth.guard';
+import { BookListComponent } from './pages/book-list/book-list.component';
+import { BorrowedBookListComponent } from './pages/borrowed-book-list/borrowed-book-list.component';
+import { MainComponent } from './pages/main/main.component';
+import { ManageBookComponent } from './pages/manage-book/manage-book.component';
+import { MyBooksComponent } from './pages/my-books/my-books.component';
+import { ReturnBooksComponent } from './pages/return-books/return-books.component';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: MainComponent,
+    canActivate: [authGuard],
+
+    children: [
+      
+        {
+          path: '',
+          component: BookListComponent,
+          canActivate: [authGuard]
+
+        },
+        {
+          path: 'my-books',
+          component: MyBooksComponent,
+          canActivate: [authGuard]
+
+        },
+        {
+          path: 'manage',
+          component: ManageBookComponent,
+          canActivate: [authGuard]
+
+        },
+        {
+          path: 'manage/:bookId',
+          component: ManageBookComponent,
+          canActivate: [authGuard]
+
+        },
+        {
+          path: 'my-borrowed-books',
+          component: BorrowedBookListComponent,
+          canActivate: [authGuard]
+
+        },
+        {
+          path: 'my-returned-books',
+          component: ReturnBooksComponent,
+          canActivate: [authGuard]
+
+        },
+      
+    ]
+
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule]
+})
+export class BookRoutingModule { }
---
+++ src/app/services/token/token.service.ts
@@ -0,0 +1,39 @@
+import { Injectable } from '@angular/core';
+import { JwtHelperService } from '@auth0/angular-jwt';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class TokenService {
+
+
+  set token(token: string){
+    localStorage.setItem('token', token);
+  }
+
+  get token(){
+    return localStorage.getItem('token') as string;
+  }
+
+  isTokenNotValid(): boolean {
+    return !this.isTokenValid();
+  }
+
+  private isTokenValid(): boolean {
+    const token: string = this.token;
+    if(!token){
+      return false;
+    }
+    //decode the token
+    const jwtHelper: JwtHelperService = new JwtHelperService();
+    //check expiry date
+    const isTokenExpired: boolean = jwtHelper.isTokenExpired(token);
+    if(isTokenExpired){
+      localStorage.clear();
+      return false
+    }
+    
+    return true;
+  }
+
+}
